d72461cabab105a0af73b2bbd5609f5b232e8d24
Santa Cadusch <santacadusch@icloud.com>
2021-04-15 16:55:11 +0200

    done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark < ApplicationRecord
-  validates :movie_id, :list_id, presence: true, uniqueness: { scope: :list_id }
+  validates :movie_id, :list_id, presence: true
+  validates :movie_id, uniqueness: { scope: [:movie_id, :list_id] }
   validates :comment, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
